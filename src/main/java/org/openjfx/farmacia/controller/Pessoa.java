b05447f760eae1a66b727117346016a35daa2280
flsantos01 <flsantos32x@outlook.com>
2021-06-14 22:22:54 -0300

    classe Pessoa.java adicionada

---
+++ src/main/java/org/openjfx/farmacia/controller/Pessoa.java
@@ -0,0 +1,31 @@
+package org.openjfx.farmacia.controller;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected String cpf;
+	protected int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
