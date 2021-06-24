79d9c2e788b5b6710418f0b556ed6839135a631a
FLAVIO <FLAVIO@DESKTOP-IEJO1AA>
2021-06-23 22:01:31 -0300

    atualização de Cliente.java

--- src/main/java/org/openjfx/farmacia/controller/cliente/Cliente.java
+++ src/main/java/org/openjfx/farmacia/controller/cliente/Cliente.java
@@ -1,7 +1,10 @@
 package org.openjfx.farmacia.controller.cliente;
 
 import org.openjfx.farmacia.Pessoa;
+import org.openjfx.farmacia.controller.produto.Produto;
 
-public class Cliente extends Pessoa {
-	
+public final class Cliente extends Pessoa {
+	public final void comprarProduto(Produto produto) {
+		
+	}
 }
