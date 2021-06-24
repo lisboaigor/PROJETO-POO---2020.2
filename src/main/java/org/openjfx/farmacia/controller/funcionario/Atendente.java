310ac4d5e6a32cf656bb9f25b670aa3083c09c03
FLAVIO <FLAVIO@DESKTOP-IEJO1AA>
2021-06-23 22:01:09 -0300

    atualização de Atendente.java

--- src/main/java/org/openjfx/farmacia/controller/funcionario/Atendente.java
+++ src/main/java/org/openjfx/farmacia/controller/funcionario/Atendente.java
@@ -1,5 +1,15 @@
 package org.openjfx.farmacia.controller.funcionario;
 
-public class Atendente extends Funcionario {
+import org.openjfx.farmacia.controller.cliente.Cliente;
+import org.openjfx.farmacia.controller.produto.Produto;
 
+public final class Atendente extends Funcionario {
+	public Atendente() {
+		super();
+	}
+
+	public void venderProduto(Cliente cliente, Produto produto) {
+		
+	}
+	
 }
