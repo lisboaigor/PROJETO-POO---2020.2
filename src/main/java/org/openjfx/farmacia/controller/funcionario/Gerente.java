a10dd99139760b79540204844274026010f77bcd
flsantos01 <flsantos32x@outlook.com>
2021-06-24 21:27:57 -0300

    atualização de Gerente.java

--- src/main/java/org/openjfx/farmacia/controller/funcionario/Gerente.java
+++ src/main/java/org/openjfx/farmacia/controller/funcionario/Gerente.java
@@ -1,17 +1,40 @@
 package org.openjfx.farmacia.controller.funcionario;
 
 public final class Gerente extends Funcionario {
-	public static void gerarRelatorio() {};
-	
-	public static void contratarFuncionario(Funcionario f) {
-		cadastrarFuncionario(f = new Atendente());
+	public Gerente() {
+		setSalario(5*salarioMinimo);
 	}
 	
-	private static void cadastrarFuncionario(Funcionario f) {
-		
+	public Gerente(String nome) {
+		setSalario(5*salarioMinimo);
+		setNome(nome);
 	}
 	
-	public static void demitirFuncionario(Funcionario f) {
-		
+	public Gerente(String nome, String cpf) {
+		setSalario(5*salarioMinimo);
+		setNome(nome);
+		setCpf(cpf);
+	}
+	
+	public Gerente(String nome, String cpf, int idade) {
+		setSalario(5*salarioMinimo);
+		setNome(nome);
+		setCpf(cpf);
+		setIdade(idade);
+	}
+	
+	@Override
+	public float getSalario() {
+		return salario;
+	}
+	
+	@Override
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return getNome() + ";" + getCpf() + ";" + getIdade() + ";" + getSalario();
 	}
 }
