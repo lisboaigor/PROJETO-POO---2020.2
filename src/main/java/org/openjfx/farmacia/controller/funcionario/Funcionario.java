9fdb59e0eb7b5adccad76948c6e14d9723f1085d
flsantos01 <flsantos32x@outlook.com>
2021-06-24 21:27:52 -0300

    atualização de Funcionario.java

--- src/main/java/org/openjfx/farmacia/controller/funcionario/Funcionario.java
+++ src/main/java/org/openjfx/farmacia/controller/funcionario/Funcionario.java
@@ -1,9 +1,11 @@
 package org.openjfx.farmacia.controller.funcionario;
 
-import org.openjfx.farmacia.Pessoa;
+import org.openjfx.farmacia.controller.Pessoa;
 
 public abstract class Funcionario extends Pessoa {
-	public Funcionario() {
-		
-	}
+	public static float salarioMinimo = 1100;
+	protected float salario;
+	
+	protected abstract float getSalario();
+	protected abstract void setSalario(float salario);
 }
