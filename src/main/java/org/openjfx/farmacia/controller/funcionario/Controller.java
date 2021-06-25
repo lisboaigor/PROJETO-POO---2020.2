d3acad7577dd2a4c0ef63288b906f3aee4654494
flsantos01 <flsantos32x@outlook.com>
2021-06-25 14:36:25 -0300

    interface Controller para a classe FuncionarioController foi criada

---
+++ src/main/java/org/openjfx/farmacia/controller/funcionario/Controller.java
@@ -0,0 +1,7 @@
+package org.openjfx.farmacia.controller.funcionario;
+
+public interface Controller {
+	public void contratarFuncionario();
+	public void demitirFuncionario(Funcionario funcionario);
+	public void obterFolhaDePagamento();
+}
