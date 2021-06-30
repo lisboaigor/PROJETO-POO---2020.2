d7e96add5a5446ee292760194410faa0c74786c0
flsantos01 <flsantos32x@outlook.com>
2021-06-30 20:51:26 -0300

    adicionando um Set de compras para a classe Cliente

--- src/main/java/org/openjfx/farmacia/controller/cliente/Cliente.java
+++ src/main/java/org/openjfx/farmacia/controller/cliente/Cliente.java
@@ -1,20 +1,29 @@
 package org.openjfx.farmacia.controller.cliente;
 
+import java.util.Set;
+
 import org.openjfx.farmacia.controller.Pessoa;
 
 public final class Cliente extends Pessoa {
+	private Set<Compra> compras;
+	
 	public Cliente(String nome) {
 		setNome(nome);
 	}
-	
+
 	public Cliente(String nome, String cpf) {
 		setNome(nome);
 		setCpf(cpf);
 	}
-	
+
 	public Cliente(String nome, String cpf, int idade) {
 		setNome(nome);
 		setCpf(cpf);
 		setIdade(idade);
+	}
+
+	@Override
+	public String toString() {
+		return nome + ";" + cpf + ";" + idade;
 	}
 }
