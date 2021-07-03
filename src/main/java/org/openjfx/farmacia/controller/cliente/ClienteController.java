da62736f3ed2e2a55c806254c82470139577c46a
flsantos01 <flsantos32x@outlook.com>
2021-07-03 18:50:24 -0300

    atualização em ClienteController

--- src/main/java/org/openjfx/farmacia/controller/cliente/ClienteController.java
+++ src/main/java/org/openjfx/farmacia/controller/cliente/ClienteController.java
@@ -1,5 +1,17 @@
 package org.openjfx.farmacia.controller.cliente;
 
-public class ClienteController {
+public class ClienteController implements Controller {
+
+	@Override
+	public void cadastrarCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removerCliente() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
