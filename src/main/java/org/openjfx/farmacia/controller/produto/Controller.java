c8f3fc3ac97cabb04a660555f01782b289e199df
lisboaigor <igorlisboa30@hotmail.com>
2021-07-05 14:25:38 -0300

    Listar produtos por categoria adicionado.

--- src/main/java/org/openjfx/farmacia/controller/produto/Controller.java
+++ src/main/java/org/openjfx/farmacia/controller/produto/Controller.java
@@ -1,10 +1,12 @@
 package org.openjfx.farmacia.controller.produto;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface Controller {
 	public void imprimirEstoque();
 	public void cadastrarProduto(ProdutoEstoque produto);
 	public void inicializarEstoque() throws IOException;
 	public void fecharEstoque();
+	public List<ProdutoEstoque> filtrarCategoria(String categoria);
 }
