66b0923ad077e861631d498b5bdd449ccc39e9d1
flsantos01 <flsantos32x@outlook.com>
2021-07-05 15:33:26 -0300

    atualização de Controller

--- src/main/java/org/openjfx/farmacia/controller/cliente/Controller.java
+++ src/main/java/org/openjfx/farmacia/controller/cliente/Controller.java
@@ -3,4 +3,7 @@ package org.openjfx.farmacia.controller.cliente;
 public interface Controller {
 	public void cadastrarCliente();
 	public void removerCliente();
+	
+	public void listarCadastrados();
+	public void listarExcluidos();
 }
