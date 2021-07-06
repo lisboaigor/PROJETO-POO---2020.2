cec47536194e54a8bc572389e1ea97f3d2ee099c
lisboaigor <igorlisboa30@hotmail.com>
2021-07-05 22:09:26 -0300

    Adicionada vizualização de produtos em tabela.

---
+++ src/main/java/org/openjfx/farmacia/controller/viewController/cenaPrincipalController.java
@@ -0,0 +1,15 @@
+package org.openjfx.farmacia.controller.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class cenaPrincipalController {
+    @FXML
+    public Button irParaCesta;
+    @FXML
+    public Button adicionarCesta;
+
+    public void adicionarProdutoCesta(Button adicionarCesta) {
+        System.out.println("Produto adicionado!");
+    }
+}
