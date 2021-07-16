fae88e003db3e5be5d70a93729cc99c599335a34
flsantos01 <flsantos32x@outlook.com>
2021-07-16 14:11:09 -0300

    InvalidCustomerException created

---
+++ src/main/java/org/openjfx/farmacia/controller/cliente/InvalidCustomerException.java
@@ -0,0 +1,7 @@
+package org.openjfx.farmacia.controller.cliente;
+
+public class InvalidCustomerException extends Exception {
+	public InvalidCustomerException(String message) {
+		super(message);
+	}	
+}
