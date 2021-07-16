5232ab3937fb26f015f57123d3e99ec9f497e172
flsantos01 <flsantos32x@outlook.com>
2021-07-16 14:11:28 -0300

    improved readibility of Produto.java

--- src/main/java/org/openjfx/farmacia/controller/produto/Produto.java
+++ src/main/java/org/openjfx/farmacia/controller/produto/Produto.java
@@ -11,24 +11,28 @@ public abstract class Produto {
     protected SimpleStringProperty formula;
     protected SimpleDoubleProperty precoUnitario;
 
+    // Codigo
     public String getCodigo() { return codigo.get(); }
     public SimpleStringProperty codigoProperty() { return codigo; }
     public void setCodigo(String codigo) { this.codigo.set(codigo); }
 
+    // Nome
     public String getNome() { return nome.get(); }
     public SimpleStringProperty nomeProperty() { return nome; }
     public void setNome(String nome) { this.nome.set(nome); }
 
+    // Fabricante
     public String getFabricante() { return fabricante.get(); }
-
     public void setFabricante(String fabricante) { this.fabricante.set(fabricante); }
 
+    // Categoria
     public String getCategoria() { return categoria.get(); }
-
     public void setCategoria(String categoria) { this.categoria.set(categoria); }
     
+    // Formula
     public String getFormula() { return formula.get(); }
 
+    // Preco Unitario
     public double getPrecoUnitario() { return precoUnitario.get(); }
     public SimpleDoubleProperty precoUnitarioProperty() { return precoUnitario; }
     public void setPrecoUnitario(double precoUnitario) { this.precoUnitario.set(precoUnitario); }
