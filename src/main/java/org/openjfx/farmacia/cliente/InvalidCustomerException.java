b873c599c6c1f5e14186daf6bc1f59876bd84a24
flsantos01 <flsantos32x@outlook.com>
2021-07-17 19:41:24 -0300

    added 2 more constructors for the InvalidCustomerException class

--- src/main/java/org/openjfx/farmacia/cliente/InvalidCustomerException.java
+++ src/main/java/org/openjfx/farmacia/cliente/InvalidCustomerException.java
@@ -3,5 +3,13 @@ package org.openjfx.farmacia.cliente;
 public class InvalidCustomerException extends Exception {
 	public InvalidCustomerException(String message) {
 		super(message);
-	}	
+	}
+	
+	public InvalidCustomerException(Throwable cause) {
+		super(cause);
+	}
+	
+	public InvalidCustomerException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
