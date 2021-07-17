1d6a6d8fd5ff43363d70abec0535eccc41ace719
flsantos01 <flsantos32x@outlook.com>
2021-07-17 19:45:54 -0300

    @SuppressWarnings("exports") added

--- src/main/java/org/openjfx/farmacia/cliente/InvalidCPFException.java
+++ src/main/java/org/openjfx/farmacia/cliente/InvalidCPFException.java
@@ -1,5 +1,6 @@
 package org.openjfx.farmacia.cliente;
 
+@SuppressWarnings("serial")
 public class InvalidCPFException extends InvalidCustomerException{
 	public InvalidCPFException(String message) {
 		super(message);
