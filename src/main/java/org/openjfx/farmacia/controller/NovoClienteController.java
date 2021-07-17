8dd37957b1bc3d406f13d6ede9e774cb43b0313b
flsantos01 <flsantos32x@outlook.com>
2021-07-17 19:42:00 -0300

    InvalidCustomerException linked to NovoClienteController

--- src/main/java/org/openjfx/farmacia/controller/NovoClienteController.java
+++ src/main/java/org/openjfx/farmacia/controller/NovoClienteController.java
@@ -5,28 +5,45 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.scene.control.TextField;
-import org.openjfx.farmacia.cliente.Cliente;
+import org.openjfx.farmacia.cliente.*;
 
 public class NovoClienteController {
-    ObservableList<Cliente> clientes;
+	public ObservableList<Cliente> clientes;
 
-    public TextField nomeClienteField;
-    public TextField cpfClienteField;
+	public TextField nomeClienteField;
+	public TextField cpfClienteField;
 
-    public void salvarNovoCliente() {
-        if (!cpfClienteField.getText().isEmpty() ||
-            cpfClienteField.getText() != null ||
-            !nomeClienteField.getText().isEmpty() ||
-            nomeClienteField.getText() != null) {
-            clientes.add(new Cliente(new SimpleStringProperty(cpfClienteField.getText()),
-                    				 new SimpleStringProperty(nomeClienteField.getText()),
-                    				 new SimpleBooleanProperty(true)));
+	public void salvarNovoCliente() throws InvalidCustomerException {
+		if (!cpfClienteField.getText().isEmpty() || cpfClienteField.getText() != null
+				|| !nomeClienteField.getText().isEmpty() || nomeClienteField.getText() != null) {
+			
+			boolean isValid = false;
+			try {
+				isValid = validaCPF(cpfClienteField.getText());
+				System.out.println("isValid = " + isValid);
+				
+				clientes.add(new Cliente(new SimpleStringProperty(cpfClienteField.getText()),
+						new SimpleStringProperty(nomeClienteField.getText()), new SimpleBooleanProperty(true)));
+			} catch (InvalidCPFException exception) {
+				System.out.println("isValid = " + isValid);
+				//exception.printStackTrace();
+				throw new InvalidCustomerException(exception);
+			}
 
-        }
-    }
+		}
+	}
 
-    public void setClientes(FilteredList<Cliente> clientes) {
-        this.clientes = (ObservableList<Cliente>) clientes.getSource();
-    }
+	private boolean validaCPF(String CPF) throws InvalidCPFException {
+		if (CPF.length() != 11)
+			throw new InvalidCPFException("CPF must be 11 characters long");
+		else if (!CPF.toLowerCase().equals(CPF.toUpperCase()))
+			throw new InvalidCPFException("CPF must have digits only");
+		else
+			return true;
+	}
+
+	public void setClientes(FilteredList<Cliente> clientes) {
+		this.clientes = (ObservableList<Cliente>) clientes.getSource();
+	}
 
 }
