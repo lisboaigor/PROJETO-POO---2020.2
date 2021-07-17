86a1acd7078ec866f15c1d880f1ed308b1970be2
flsantos01 <flsantos32x@outlook.com>
2021-07-17 19:39:49 -0300

    Controller.java deleted

--- src/main/java/org/openjfx/farmacia/cliente/Controller.java
+++
@@ -1,9 +0,0 @@
-package org.openjfx.farmacia.cliente;
-
-public interface Controller {
-	public void cadastrarCliente();
-	public void removerCliente();
-	
-	public void listarCadastrados();
-	public void listarExcluidos();
-}
