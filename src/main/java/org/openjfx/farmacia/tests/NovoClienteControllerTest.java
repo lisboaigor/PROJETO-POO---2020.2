85a3e56529a41eb4bb4234fbaed596b41bb61904
flsantos01 <flsantos32x@outlook.com>
2021-07-18 00:44:44 -0300

    NovoClientController unit testing completed

---
+++ src/main/java/org/openjfx/farmacia/tests/NovoClienteControllerTest.java
@@ -0,0 +1,22 @@
+package org.openjfx.farmacia.tests;
+
+import org.junit.Test;
+import org.openjfx.farmacia.cliente.InvalidCPFException;
+import org.openjfx.farmacia.controller.NovoClienteController;
+
+import static org.junit.Assert.assertTrue;
+
+public class NovoClienteControllerTest {
+	@Test
+	public void validaCPFTest() {
+		try {
+			assertTrue(NovoClienteController.validarCPF("12345678912"));
+			assertTrue(NovoClienteController.validarCPF("1234567891a"));
+			assertTrue(NovoClienteController.validarCPF("1234567891A"));
+			assertTrue(NovoClienteController.validarCPF("1234567891"));
+		}
+		catch(InvalidCPFException exception) {
+			exception.printStackTrace();
+		}
+	}
+}
