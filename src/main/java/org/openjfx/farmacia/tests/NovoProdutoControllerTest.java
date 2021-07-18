1ca4032cdafc35534444d3407039257f2e3b52de
flsantos01 <flsantos32x@outlook.com>
2021-07-18 13:42:03 -0300

    NovoProdutoController created

---
+++ src/main/java/org/openjfx/farmacia/tests/NovoProdutoControllerTest.java
@@ -0,0 +1,13 @@
+package org.openjfx.farmacia.tests;
+
+import org.junit.Test;
+import org.openjfx.farmacia.controller.NovoProdutoController;
+
+import static org.junit.Assert.assertTrue;
+
+public class NovoProdutoControllerTest {
+	@Test
+	public void validaProdutoTest() {
+		
+	}
+}
