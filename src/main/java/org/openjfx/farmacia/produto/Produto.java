837277ddf0d6ae797350ef7ca9a292b03126da5b
flsantos01 <flsantos32x@outlook.com>
2021-07-18 00:27:25 -0300

    code formatting

--- src/main/java/org/openjfx/farmacia/produto/Produto.java
+++ src/main/java/org/openjfx/farmacia/produto/Produto.java
@@ -4,39 +4,75 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public abstract class Produto {
-    protected SimpleStringProperty codigo;
-    protected SimpleStringProperty nome;
-    protected SimpleStringProperty fabricante;
-    protected SimpleStringProperty categoria;
-    protected SimpleStringProperty formula;
-    protected SimpleDoubleProperty precoUnitario;
+	protected SimpleStringProperty codigo;
+	protected SimpleStringProperty nome;
+	protected SimpleStringProperty fabricante;
+	protected SimpleStringProperty categoria;
+	protected SimpleStringProperty formula;
+	protected SimpleDoubleProperty precoUnitario;
 
-    // Codigo
-    public String getCodigo() { return codigo.get(); }
-    @SuppressWarnings("exports")
-	public SimpleStringProperty codigoProperty() { return codigo; }
-    public void setCodigo(String codigo) { this.codigo.set(codigo); }
+	// Codigo
+	public String getCodigo() {
+		return codigo.get();
+	}
 
-    // Nome
-    public String getNome() { return nome.get(); }
-    @SuppressWarnings("exports")
-	public SimpleStringProperty nomeProperty() { return nome; }
-    public void setNome(String nome) { this.nome.set(nome); }
+	@SuppressWarnings("exports")
+	public SimpleStringProperty codigoProperty() {
+		return codigo;
+	}
 
-    // Fabricante
-    public String getFabricante() { return fabricante.get(); }
-    public void setFabricante(String fabricante) { this.fabricante.set(fabricante); }
+	public void setCodigo(String codigo) {
+		this.codigo.set(codigo);
+	}
 
-    // Categoria
-    public String getCategoria() { return categoria.get(); }
-    public void setCategoria(String categoria) { this.categoria.set(categoria); }
-    
-    // Formula
-    public String getFormula() { return formula.get(); }
+	// Nome
+	public String getNome() {
+		return nome.get();
+	}
 
-    // Preco Unitario
-    public double getPrecoUnitario() { return precoUnitario.get(); }
-    @SuppressWarnings("exports")
-	public SimpleDoubleProperty precoUnitarioProperty() { return precoUnitario; }
-    public void setPrecoUnitario(double precoUnitario) { this.precoUnitario.set(precoUnitario); }
+	@SuppressWarnings("exports")
+	public SimpleStringProperty nomeProperty() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome.set(nome);
+	}
+
+	// Fabricante
+	public String getFabricante() {
+		return fabricante.get();
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante.set(fabricante);
+	}
+
+	// Categoria
+	public String getCategoria() {
+		return categoria.get();
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria.set(categoria);
+	}
+
+	// Formula
+	public String getFormula() {
+		return formula.get();
+	}
+
+	// Preco Unitario
+	public double getPrecoUnitario() {
+		return precoUnitario.get();
+	}
+
+	@SuppressWarnings("exports")
+	public SimpleDoubleProperty precoUnitarioProperty() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(double precoUnitario) {
+		this.precoUnitario.set(precoUnitario);
+	}
 }
