c04c963c0e9f56c88d5128ef090ef52f26f7bfdd
flsantos01 <flsantos32x@outlook.com>
2021-07-18 00:16:02 -0300

    @SuppressWarnings("exports")

--- src/main/java/org/openjfx/farmacia/App.java
+++ src/main/java/org/openjfx/farmacia/App.java
@@ -9,7 +9,8 @@ import org.openjfx.farmacia.controller.MainController;
 import java.io.IOException;
 
 public class App extends Application {
-    @Override
+    @SuppressWarnings("exports")
+	@Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main" + ".fxml"));
         Scene scene = new Scene(fxmlLoader.load());
