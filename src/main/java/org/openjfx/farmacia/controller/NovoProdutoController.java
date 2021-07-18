e0715134d5e34868fada5178dcb740feaa183c9f
flsantos01 <flsantos32x@outlook.com>
2021-07-18 13:41:41 -0300

    code formatting

--- src/main/java/org/openjfx/farmacia/controller/NovoProdutoController.java
+++ src/main/java/org/openjfx/farmacia/controller/NovoProdutoController.java
@@ -9,47 +9,49 @@ import javafx.scene.control.TextField;
 import org.openjfx.farmacia.produto.ProdutoEstoque;
 
 public class NovoProdutoController {
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoFormula;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoQuantidade;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoCategoria;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoFabricante;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoNome;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoCodigo;
-    @SuppressWarnings("exports")
+	@SuppressWarnings("exports")
 	public TextField campoPreco;
 
-    ObservableList<ProdutoEstoque> estoque;
+	public ObservableList<ProdutoEstoque> estoque;
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void setEstoque(FilteredList<ProdutoEstoque> estoque) {
-        this.estoque = (ObservableList<ProdutoEstoque>) estoque.getSource();
-    }
+		this.estoque = (ObservableList<ProdutoEstoque>) estoque.getSource();
+	}
 
-    public void salvarNovoProduto() {
-        if ((!campoCodigo.getText().isEmpty() || campoCategoria.getText() != null) &&
-                (!campoNome.getText().isEmpty() || campoNome.getText() != null) &&
-                (!campoFabricante.getText().isEmpty() || campoFabricante.getText() != null) &&
-                (!campoCategoria.getText().isEmpty() || campoCategoria.getText() != null) &&
-                (!campoFormula.getText().isEmpty() || campoFormula.getText() != null) &&
-                (!campoQuantidade.getText().isEmpty() || campoQuantidade.getText() != null) &&
-                (!campoPreco.getText().isEmpty() || campoQuantidade.getText() != null)) {
-            estoque.add(new ProdutoEstoque(new SimpleStringProperty(campoCodigo.getText()),
-                        new SimpleStringProperty(campoNome.getText()), new SimpleStringProperty(campoFabricante.getText()),
-                        new SimpleStringProperty(campoCategoria.getText()), new SimpleStringProperty(campoFormula.getText()),
-                        new SimpleDoubleProperty(Double.parseDouble(campoPreco.getText())), new SimpleIntegerProperty(Integer.parseInt(campoQuantidade.getText()))));
-            campoCodigo.clear();
-            campoNome.clear();
-            campoFabricante.clear();
-            campoCategoria.clear();
-            campoFormula.clear();
-            campoQuantidade.clear();
-            campoPreco.clear();
-        }
-    }
+	public void salvarNovoProduto() {
+		if ((!campoCodigo.getText().isEmpty() || campoCategoria.getText() != null)
+				&& (!campoNome.getText().isEmpty() || campoNome.getText() != null)
+				&& (!campoFabricante.getText().isEmpty() || campoFabricante.getText() != null)
+				&& (!campoCategoria.getText().isEmpty() || campoCategoria.getText() != null)
+				&& (!campoFormula.getText().isEmpty() || campoFormula.getText() != null)
+				&& (!campoQuantidade.getText().isEmpty() || campoQuantidade.getText() != null)
+				&& (!campoPreco.getText().isEmpty() || campoQuantidade.getText() != null)) {
+			estoque.add(new ProdutoEstoque(new SimpleStringProperty(campoCodigo.getText()),
+					new SimpleStringProperty(campoNome.getText()), new SimpleStringProperty(campoFabricante.getText()),
+					new SimpleStringProperty(campoCategoria.getText()),
+					new SimpleStringProperty(campoFormula.getText()),
+					new SimpleDoubleProperty(Double.parseDouble(campoPreco.getText())),
+					new SimpleIntegerProperty(Integer.parseInt(campoQuantidade.getText()))));
+			campoCodigo.clear();
+			campoNome.clear();
+			campoFabricante.clear();
+			campoCategoria.clear();
+			campoFormula.clear();
+			campoQuantidade.clear();
+			campoPreco.clear();
+		}
+	}
 }
