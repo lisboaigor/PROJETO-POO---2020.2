b51d7b049478b314f76416306f89b92f009b1ecf
flsantos01 <flsantos32x@outlook.com>
2021-07-18 21:06:31 -0300

    NovoProdutoController unit testing finished

--- src/main/java/org/openjfx/farmacia/tests/NovoProdutoControllerTest.java
+++ src/main/java/org/openjfx/farmacia/tests/NovoProdutoControllerTest.java
@@ -1,13 +1,25 @@
 package org.openjfx.farmacia.tests;
 
 import org.junit.Test;
-import org.openjfx.farmacia.controller.NovoProdutoController;
+import org.openjfx.farmacia.produto.InvalidProductException;
+import org.openjfx.farmacia.produto.NegativeAmountException;
+import org.openjfx.farmacia.produto.NegativePriceException;
 
 import static org.junit.Assert.assertTrue;
+import static org.openjfx.farmacia.controller.NovoProdutoController.validaProduto;
 
 public class NovoProdutoControllerTest {
 	@Test
-	public void validaProdutoTest() {
-		
+	public void validaProdutoTest() throws InvalidProductException {
+		try {
+			assertTrue(validaProduto(-200.00, 20));
+			assertTrue(validaProduto(200.00, -20));
+			assertTrue(validaProduto(200.00, 20));
+		} catch (NegativePriceException exception) {
+			throw new InvalidProductException(exception);
+		} catch (NegativeAmountException exception) {
+			throw new InvalidProductException(exception);
+		}
+
 	}
 }
